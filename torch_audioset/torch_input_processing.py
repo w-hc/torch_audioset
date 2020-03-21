974b5571308645a8ce499015d15201824c7042a0
Haochen Wang <wanghaochen@uchicago.edu>
2020-03-21 14:04:36 -0500

    standardize torch processing; explain use of sqrt on fft outputs

--- torch_audioset/torch_input_processing.py
+++ torch_audioset/torch_input_processing.py
@@ -24,34 +24,37 @@ class WaveformToInput():
             f_max=CommonParams.MEL_MAX_HZ,
             n_mels=CommonParams.NUM_MEL_BANDS
         )
+        # note that the STFT filtering logic is exactly the same as that of a
+        # conv kernel. It is the center of the kernel, not the left edge of the
+        # kernel that is aligned at the start of the signal.
 
     def __call__(self, waveform, sample_rate):
         '''
         Args:
-            waveform: torch tsr [1, num_channels, num_steps]
+            waveform: torch tsr [num_audio_channels, num_time_steps]
             sample_rate: per second sample rate
+        Returns:
+            batched torch tsr of shape [N, C, T]
         '''
-        print("please confirm whether wave is torch or numpy tsr: currently {}".format(type(waveform)))
-        waveform = waveform.squeeze(axis=0)
-
-        # init the ops
-        resampler = ta_trans.Resample(sample_rate, CommonParams.TARGET_SAMPLE_RATE)
+        waveform = torch.from_numpy(waveform)
+        # TODO move the waveform data to GPU and see if there is a speed boost
 
         x = waveform.mean(axis=0, keepdims=True)  # average over channels
+        resampler = ta_trans.Resample(sample_rate, CommonParams.TARGET_SAMPLE_RATE)
         x = resampler(x)
-        x = self.mel_trans_ope(x)  # [1, num_freq, time_steps]
-        x = x.squeeze(dim=0).T  # [time_steps, num_freq]
+        x = self.mel_trans_ope(x)
+        x = x.squeeze(dim=0)  # # [1, C, T] -> [C, T]
 
         window_size_in_frames = int(round(
             CommonParams.PATCH_WINDOW_IN_SECONDS / CommonParams.STFT_HOP_LENGTH_SECONDS
         ))
+        num_chunks = x.shape[1] // window_size_in_frames
 
         # reshape into chunks of non-overlapping sliding window
-        length = len(x)
-        num_chunks = length // window_size_in_frames
         num_frames_to_use = num_chunks * window_size_in_frames
-        x = x[:num_frames_to_use, :]
-        x = x.reshape(num_chunks, window_size_in_frames, x.shape[-1])
+        x = x[:, :num_frames_to_use]
+        x = x.reshape(-1, num_chunks, window_size_in_frames)
+        x = x.permute(1, 0, 2)  # [C, N, T] ->  [N, C, T]
         return x
 
 
@@ -69,7 +72,14 @@ class VGGishLogMelSpectrogram(ta_trans.MelSpectrogram):
             torch.Tensor: Mel frequency spectrogram of size (..., ``n_mels``, time)
         """
         specgram = self.spectrogram(waveform)
-        specgram = specgram ** 0.5  # TODO: document this hack later!!
+        # NOTE at mel_features.py:98, googlers used np.abs on fft output and
+        # as a result, the output is just the norm of spectrogram raised to power 1
+        # For torchaudio.MelSpectrogram, however, the default
+        # power for its spectrogram is 2.0. Hence we need to sqrt it.
+        # I can change the power arg at the constructor level, but I don't
+        # want to make the code too dirty
+        specgram = specgram ** 0.5
+
         mel_specgram = self.mel_scale(specgram)
         mel_specgram = torch.log(mel_specgram + CommonParams.LOG_OFFSET)
         return mel_specgram
